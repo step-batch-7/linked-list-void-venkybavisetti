ebec21f837dd96b31b261c623585985698ec1245
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-05-16 09:24:39 +0530

    modified tests

--- test/test_code.c
+++ test/test_code.c
@@ -1,5 +1,7 @@
+#include <math.h>
 #include "../linkedlist.h"
 #include "test.h"
+#include "test_code.h"
 
 Element create_int_element(int value)
 {
@@ -8,22 +10,33 @@ Element create_int_element(int value)
   return element;
 }
 
-int assert_integers(int actual, int expected)
+Status assert_array(List_ptr actual_array, Array expected_array)
 {
 
-  return actual == expected;
+  Status status = Success;
+  Node_ptr p_walk = actual_array->first;
+
+  for (int i = 0; i < fmax(actual_array->length, expected_array.length); i++)
+  {
+    if (expected_array.values[i] != *(int *)p_walk->element)
+      status = Failure;
+    p_walk = p_walk->next;
+  }
+  return status;
 }
 
 void test_add_to_list(Result *result)
 {
   List_ptr list = create_list();
-  Status actual = add_to_list(list, create_int_element(3));
-  Status status = assert_integers(actual, Success);
-  status = status && assert_integers(list->length, 1);
+  add_to_list(list, create_int_element(3));
+  int firest_test[] = {3};
+  Array first_expected = {firest_test, 1};
+  Status status = assert_array(list, first_expected);
   print_result("Should add at first when list is empty", status, result);
 
-  actual = add_to_list(list, create_int_element(4));
-  status = assert_integers(actual, Success);
-  status = status && assert_integers(list->length, 2);
+  add_to_list(list, create_int_element(4));
+  int second_test[] = {3, 4};
+  Array second_expected = {second_test, 2};
+  status = assert_array(list, second_expected);
   print_result("Should add the element at the end when list is not empty", status, result);
 }
--- test/test_code.h
+++ test/test_code.h
@@ -1,6 +1,15 @@
 #ifndef __TEST_CODE__H
 #define __TEST_CODE__H
 
+typedef struct
+{
+  int *values;
+  int length;
+
+} Array;
+
+typedef Array *Array_ptr;
+
 void test_add_to_list(Result *result);
 
 #endif
